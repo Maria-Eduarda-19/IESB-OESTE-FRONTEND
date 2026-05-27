4769fb97dad0a76a1d2d7168814982fd553899b5
Maria Eduarda Fernandes Calado <dudacalado2006@gmail.com>
2026-05-27 10:41:29 -0300

    Atividade02

---
+++ praticas/pratica04/Atividade02/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <p>Esse é o meu aplicativo</p>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
